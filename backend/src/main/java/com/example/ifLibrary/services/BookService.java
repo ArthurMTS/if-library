775c376c796fefbb1bb580e47c6d1988eadc9ab1
Arthur Silva <73958393+ArthurMTS@users.noreply.github.com>
2025-08-08 14:21:08 -0300

    feat(backend): service database operations

---
+++ backend/src/main/java/com/example/ifLibrary/services/BookService.java
@@ -0,0 +1,50 @@
+package com.example.ifLibrary.services;
+
+import com.example.ifLibrary.DTOs.BookDTO;
+import com.example.ifLibrary.models.BookModel;
+import com.example.ifLibrary.repositories.BookRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class BookService {
+    @Autowired
+    private BookRepository repository;
+
+    public List<BookModel> getAll() {
+        return this.repository.findAll();
+    }
+
+    public BookModel getOne(UUID id) {
+        var bookO = this.repository.findById(id);
+        if (bookO.isEmpty()) {
+            throw new EntityNotFoundException("Book not found with id: " + id);
+        }
+        return bookO.get();
+    }
+
+    public BookModel create(BookDTO data) {
+        var book = new BookModel(data);
+        return this.repository.save(book);
+    }
+
+    public BookModel update(UUID id, BookDTO data) {
+        var book = getOne(id);
+        book.setTitle(data.title());
+        book.setHeaderImage(data.headerImage());
+        book.setPlayLink(data.playLink());
+        book.setBlogLink(data.blogLink());
+        book.setFinished(data.finished());
+        book.setTags(data.tags());
+        return this.repository.save(book);
+    }
+
+    public void delete(UUID id) {
+        var book = getOne(id);
+        this.repository.delete(book);
+    }
+}
