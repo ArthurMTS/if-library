ac225f619fad92307c88fd942b32a3618acaf291
Arthur Silva <73958393+ArthurMTS@users.noreply.github.com>
2025-08-08 09:12:09 -0300

    (backend): initial commit

---
+++ backend/src/main/java/com/example/ifLibrary/IfLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.ifLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfLibraryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/ifLibrary/IfLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ifLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
