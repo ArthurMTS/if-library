40ee1b0ccdd7181d54b3e2daa34bafd35038a2e5
Arthur Silva <73958393+ArthurMTS@users.noreply.github.com>
2025-08-14 15:44:32 -0300

    feat(frontend): initial structure of the page

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Header } from './components/header/header';
+import { Sidebar } from './components/sidebar/sidebar';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Sidebar, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  
 }
