cf6936f463eb4b8c78c30e96c89ac353c074d438
Arthur Silva <73958393+ArthurMTS@users.noreply.github.com>
2025-08-18 08:30:08 -0300

    feat(frontend): card component

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './components/header/header';
 import { Sidebar } from './components/sidebar/sidebar';
 import { Footer } from './components/footer/footer';
+import { Card } from './components/card/card';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Sidebar, Footer],
+  imports: [Header, Sidebar, Footer, Card],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
